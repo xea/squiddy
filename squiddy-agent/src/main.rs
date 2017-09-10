bdee818c89c71f27d30da5185982ee4ac01496fc
Alex Pecsi <alex.pecsi@gmail.com>
2017-09-10 17:10:07 +0100

    Initial work on client

--- squiddy-agent/src/main.rs
+++ squiddy-agent/src/main.rs
@@ -1,3 +1,79 @@
+extern crate bytes;
+
+use bytes::BytesMut;
+
+pub trait Action {
+    fn perform(&mut self);
+}
+
+pub struct IncrementCounter {
+    name: String
+}
+
+impl Action for IncrementCounter {
+
+    fn perform(&mut self) {
+
+    }
+
+}
+
+pub trait Matcher {
+    fn accept(&self, bytes: &mut BytesMut) -> bool;
+}
+
+pub struct LengthMatcher {
+    threshold: usize
+}
+
+impl Matcher for LengthMatcher {
+    fn accept(&self, bytes: &mut BytesMut) -> bool {
+        bytes.len() >= self.threshold
+    }
+}
+
+pub struct AnyMatcher {
+    children: Vec<Box<Matcher>>
+}
+
+impl Matcher for AnyMatcher {
+
+    fn accept(&self, bytes: &mut BytesMut) -> bool {
+        for child in self.children.iter() {
+            if child.accept(bytes) {
+                return true;
+            }
+        }
+
+        false
+    }
+}
+
+pub struct Condition {
+    matcher: Box<Matcher>,
+    actions: Vec<Box<Action>>
+}
+
 fn main() {
-    println!("Hello, world!");
+    let input_lines = vec![
+        "Hello world",
+        "Delimited | Hello world",
+        "Hello world,in,CSV,format",
+    ];
+
+    let mut conditions = vec![
+        Condition { matcher: Box::new(AnyMatcher { children: vec![] }), actions: vec![ Box::new(IncrementCounter { name: String::from("asdf") }) ] }
+    ];
+
+    let mut bytes = BytesMut::with_capacity(32);
+
+    for line in input_lines {
+        for condition in &mut conditions {
+            if condition.matcher.accept(&mut bytes) {
+                for action in &mut condition.actions {
+                    action.perform();
+                }
+            }
+        }
+    }
 }
