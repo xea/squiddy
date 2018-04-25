0985351dbabb528ad6212c967b5de61acac10869
Alex Pecsi <alex.pecsi@gmail.com>
2018-04-25 11:01:24 +0100

    The whole future thingy started working, barely

--- squiddy-server/src/server/mod.rs
+++ squiddy-server/src/server/mod.rs
@@ -3,7 +3,8 @@ use std::sync::{ Arc, RwLock };
 use std::thread::{ JoinHandle, spawn };
 use ::config::ServerConfig;
 use ::state::State;
-use tokio::prelude::{ Async, Future, Stream };
+use bytes::BytesMut;
+use tokio::prelude::{ Async, AsyncRead, Future, Poll, Stream };
 use tokio::net::{ TcpStream, TcpListener };
 use tokio::io;
 
@@ -25,10 +26,17 @@ impl Server {
             let server = listener.incoming().for_each(move |socket| {
                 println!("New connection: {:?}", socket.peer_addr().unwrap());
 
-                let connection = AcceptClient::new(socket).then(|_| {
+                let codec = ClientCodec::new(socket);
+
+                let connection = codec.into_future()
+                    .map_err(|_| ())
+                    .map(|_| ());
+/*
+                let connection = TestFuture::new(socket).then(|_| {
                     println!("Accepted and stuff");
                     Ok(())
-                });
+                }).map_err(|_| ());
+                */
 
                 ::tokio::spawn(connection);
 
@@ -56,6 +64,97 @@ impl Server {
         }
     }
 }
+
+struct ClientCodec {
+    stream: TcpStream,
+    read_buffer: BytesMut,
+    write_buffer: BytesMut
+}
+
+impl ClientCodec {
+    pub fn new(stream: TcpStream) -> Self {
+        Self { 
+            stream,
+            read_buffer: BytesMut::new(),
+            write_buffer: BytesMut::new()
+        }
+    }
+
+    fn read_next_packet(&mut self) -> Poll<(), io::Error> {
+        loop {
+            self.read_buffer.reserve(1024);
+
+            let n = try_ready!(self.stream.read_buf(&mut self.read_buffer));
+
+            println!("Got value from try_ready: {}", n);
+
+            if n == 0 {
+                return Ok(Async::Ready(()))
+            }
+        }
+    }
+}
+
+impl Stream for ClientCodec {
+
+    type Item = ClientMessage;
+    type Error = io::Error;
+
+    fn poll(&mut self) -> Poll<Option<Self::Item>, Self::Error> {
+        let sock_closed = self.read_next_packet()?.is_ready();
+
+        println!("- read next {:?} {:?}", sock_closed, self.read_buffer.len());
+
+        if self.read_buffer.len() >= 10 {
+            let msg_code = self.read_buffer.split_to(2);
+
+            match &msg_code[..] {
+                b"he" => println!("Hello request"),
+                _ => println!("Unknown package")
+
+            }
+
+            return Ok(Async::Ready(Some(ClientMessage::Noop)))
+        }
+
+        if sock_closed {
+            Ok(Async::Ready(None))
+        } else {
+            Ok(Async::NotReady)
+        }
+    }
+}
+
+enum ClientMessage {
+    Noop
+}
+
+/*
+struct TestFuture {
+    inner: Box<Future<Item=(), Error=()>>
+}
+
+impl TestFuture {
+    pub fn new(stream: TcpStream) -> Self {
+        Self {
+            inner: Box::new(io::write_all(stream, "HELLO\n").map(|_| ()).map_err(|_| ()))
+        }
+    }
+}
+
+impl Future for TestFuture {
+    type Item = ();
+    type Error = ();
+
+    fn poll(&mut self) -> Result<Async<Self::Item>, Self::Error> {
+        let v = try_ready!(self.inner.poll());
+        Ok(Async::Ready(()))
+
+        //try_ready!(self.inner.poll())
+    }
+    
+}
+*/
 
 enum ClientState {
     NewConnection(TcpStream),
